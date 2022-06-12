18482e338142af3beb03ab5786776cedece8b599
ademtarhan <ademtarhan.dev@gmail.com>
2022-06-12 15:35:13 +0300

    tools added and defined

---
+++ Part1/Part1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Part1
+//
+//  Created by Adem Tarhan on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textFirstNumber: UITextField!
+    @IBOutlet weak var textSecondNumber: UITextField!
+    
+    @IBOutlet weak var labelResult: UILabel!
+    
+    var buttons : [UIButton]!
+    
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func buttonPlus(_ sender: Any) {
+    }
+    @IBAction func buttonMinus(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonDivide(_ sender: Any) {
+    }
+    
+    @IBAction func buttonMultiply(_ sender: Any) {
+    }
+    
+}
+
