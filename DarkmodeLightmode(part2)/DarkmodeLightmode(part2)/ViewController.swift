a7074e84ee8a751c4c5e598c38bd5cb6bb1a8cb3
ademtarhan <ademtarhan.dev@gmail.com>
2022-06-13 12:21:10 +0300

    tools added and defined(part2)

---
+++ DarkmodeLightmode(part2)/DarkmodeLightmode(part2)/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DarkmodeLightmode(part2)
+//
+//  Created by Adem Tarhan on 13.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    @IBOutlet weak var imageMode: UIImageView!
+    
+    @IBOutlet weak var switchMode: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
