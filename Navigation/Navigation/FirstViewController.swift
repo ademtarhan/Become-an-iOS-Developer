657db9a3dc0ff4f6577f9e44645e97479bf5858d
ademtarhan <ademtarhan.dev@gmail.com>
2022-06-18 16:26:08 +0300

    commit-switch between pages

---
+++ Navigation/Navigation/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Adem Tarhan on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
